470a541c1aeb9d89289439309f384253d448f78a
Nely <NAICP_14@YAHOO.COM.AR>
2015-06-09 14:50:45 -0300

    subastas_jirani

--- subastas_jirani/ayuda.php
+++ subastas_jirani/ayuda.php
@@ -1,9 +1,3 @@
-		     <li><a href="#">Muebles</a></li>
-			 <li><a href="#">Juegos</a></li>
-			 <li><a href="#">Joyas y Relojes</a></li>
-	       </ul>
-		 </li>
-   </ul>		
 <?php
    require_once("conexion.php");
    session_start();
